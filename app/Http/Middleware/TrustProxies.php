ef769e0ed97bd10710f18918ebf58b4ecccbd4e0
Nuria Vázquez <nuriavadevs@gmail.com>
2024-03-22 15:21:03 +0100

    añadir carpeta assets en resource

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -13,6 +13,7 @@ class TrustProxies extends Middleware
      * @var array<int, string>|string|null
      */
     protected $proxies;
+    
 
     /**
      * The headers that should be used to detect proxies.
@@ -25,4 +26,6 @@ class TrustProxies extends Middleware
         Request::HEADER_X_FORWARDED_PORT |
         Request::HEADER_X_FORWARDED_PROTO |
         Request::HEADER_X_FORWARDED_AWS_ELB;
+
+        
 }
