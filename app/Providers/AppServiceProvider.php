b97b8b965858d0d86875f172e3c79e271606a987
Nuria Vázquez <nuriavadevs@gmail.com>
2024-03-23 12:10:25 +0100

    cambio ruta production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,12 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
+
+    
     /**
      * Register any application services.
      */
@@ -19,6 +22,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        
         //
+        $env = config('app.env');
+
+        if ($env === 'production') {
+        URL::forceScheme('https');
+        }
+        
+        if ($env === 'production') {
+        $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
